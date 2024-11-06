8245216ce8156413b4c8a3e5073e88da644f82a0
Mans72 <romanmans12@ukr.net>
2024-11-06 17:34:51 +0100

    Add files via upload

---
+++ CowsBulls.cs
@@ -0,0 +1,181 @@
+﻿// 5040
+namespace MiniGames_con {
+
+	internal static partial class CowsBulls {
+		private static readonly List<string> options = [
+			"You Guess Random Number",
+			"Bot Guesses Your Number",
+			"Bot Guesses, You Enter Results",
+			"Exit",
+		];
+		private static int selected = -1;
+		private static int tempselected = -1;
+
+		public static void Start() {
+			Initialization();
+
+			do {
+				RenderMenu();
+
+				MouseButton button;
+				do {
+					(XY pos, button) = MouseClickHandler.GetMouse();
+
+					if (pos.X >= 5 && pos.X <= 38) {
+						if (pos.Y >= 6 && pos.Y <= 8) selected = 0;
+						else if (pos.Y >= 10 && pos.Y <= 12) selected = 1;
+						else if (pos.Y >= 14 && pos.Y <= 16) selected = 2;
+						else if (pos.Y >= 18 && pos.Y <= 20) selected = 3;
+						else selected = -1;
+					}
+					else selected = -1;
+
+					if (tempselected != selected && selected != -1) {
+						RenderSelected(selected);
+						tempselected = selected;
+					}
+
+					if (tempselected != selected && tempselected != -1) {
+						RenderUnSelected(tempselected);
+						tempselected = selected;
+					}
+
+				} while (selected == -1 || button != MouseButton.LeftButton);
+
+				Game game = new Game();
+				switch (selected) {
+					case 0:
+						game.Start();
+						break;
+					case 1:
+						game.StartBot();
+						break;
+					case 2:
+						game.StartBotBlindly();
+						break;
+					case 3:
+						Program.ReturnToMenu = true;
+						break;
+				}
+
+				if (!Program.ReturnToMenu)
+					MouseClickHandler.GetMouseClick();
+			} while (!Program.ReturnToMenu);
+		}
+
+
+
+		private class Game {
+			private XY result;
+			private int tryes = 0;
+			private string input = "";
+			private string number = "";
+			private readonly Random rand = new();
+			private List<string> vars = [];
+
+			public void Start() {
+				number = GenRandNum();
+				RenderStart("Enter a number and get the result:");
+				do {
+					RenderTryMessage(tryes);
+					input = RenderRead(tryes);
+
+					result = new(0, 0);
+					for (int i = 0; i < 4; i++)
+						if (number.Contains(input[i]))
+							if (number[i] == input[i]) result.X++;
+							else result.Y++;
+
+					RenderResult(tryes, result);
+					tryes++;
+				} while (result.X != 4);
+
+				RenderWin(tryes, $"YOU WON!    Tryes: {tryes}");
+			}
+
+			public void StartBot() {
+				vars = GenUniqueDigitNumbers();
+				RenderStart("Enter a number:       ");
+				number = RenderReadForBot();
+				do {
+					if (tryes != 0) vars = FilteringVars(vars);
+					input = vars[rand.Next(vars.Count)];
+
+					result = new(0, 0);
+					for (int i = 0; i < 4; i++) {
+						if (number.Contains(input[i]))
+							if (number[i] == input[i]) result.X++;
+							else result.Y++;
+					}
+					tryes++;
+					RenderBotTryMessage(tryes, input, result, vars.Count);
+					Thread.Sleep(200);
+				} while (result.X != 4);
+
+				RenderWin(tryes, $"I won in {tryes} tries!");
+			}
+
+			public void StartBotBlindly() {
+				vars = GenUniqueDigitNumbers();
+				RenderStart("Bot trying to guess a number:");
+				do {
+					try {
+						if (tryes != 0) vars = FilteringVars(vars);
+						input = vars[rand.Next(vars.Count)];
+						tryes++;
+					}
+					catch (System.ArgumentOutOfRangeException) {
+						RenderWin(tryes, "You are deceiving me!");
+						tryes = 0;
+						break;
+					}
+					RenderBotTryMessage(tryes, input, vars.Count);
+
+					if (result.X != 4)
+						result = RenderResultForBot(tryes);
+				} while (result.X != 4);
+
+				if (tryes != 0)
+					RenderWin(tryes, $"I won in {tryes} tries!");
+			}
+
+
+			private static string GenRandNum() {
+				Random rand = new();
+				string dig, number = "";
+				do {
+					dig = rand.Next(0, 10).ToString();
+					if (!number.Contains(dig))
+						number += dig;
+				} while (number.Length != 4);
+				return number;
+			}
+			private static List<string> GenUniqueDigitNumbers() {
+				List<string> result = [];
+				string number;
+				for (int i = 0; i < 10000; i++) {
+					number = i.ToString("0000");
+					if (Sources.HasUniqueDigits(number, 4))
+						result.Add(number);
+				}
+				return result;
+			}
+			private List<string> FilteringVars(List<string> vars) {
+				List<string> temp = [];
+				for (int i = 0; i < vars.Count; i++)
+					if (result == CheckCorrect(vars[i]))
+						temp.Add(vars[i]);
+				return temp;
+			}
+			private XY CheckCorrect(string correct) {
+				XY check = new();
+				for (int i = 0; i < 4; i++) {
+					if (correct.Contains(input[i]))
+						if (correct[i] == input[i]) check.X++;
+						else check.Y++;
+				}
+				return check;
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MiniGames_con {
+	internal partial class Program {
+		private static readonly List<string> games = [
+			"Sea Battle",//		+
+			"Minesweeper",//	+
+			"Tic Tac Toe",//	+
+			"Fifteens",//		+
+			"Cows & Bulls",//	+
+			"Tetris (Soon)",//	
+			"Snake (Soon)",//	
+		];
+		private static int selected = -1;
+		private static int tempselected = -1;
+
+		private delegate void StartGame();
+		internal static bool ReturnToMenu = false;
+
+		internal static void Main() {
+
+			while (true) {
+				Initialization();
+				RenderMenu();
+
+				int x = -1, y = -1;
+				XY pos;
+				MouseButton button;
+				do {
+					(pos, button) = MouseClickHandler.GetMouse();
+
+					if (pos.Y >= 5 && pos.Y <= 7) y = 0;
+					else if (pos.Y >= 9 && pos.Y <= 11) y = 1;
+					else if (pos.Y >= 13 && pos.Y <= 15) y = 2;
+					else y = -1;
+
+					if (pos.X >= 2 && pos.X <= 17) x = 0;
+					else if (pos.X >= 20 && pos.X <= 35) x = 1;
+					else if (pos.X >= 38 && pos.X <= 53) x = 2;
+					else x = -1;
+
+					if (x != -1 && y != -1) {
+						selected = y * 3 + x;
+						if (selected >= games.Count) selected = -1;
+
+						if (tempselected != selected && selected != -1)
+							RenderSelected(selected);
+
+						tempselected = selected;
+					}
+					else selected = -1;
+
+					if (tempselected != selected && tempselected != -1) {
+						RenderUnSelected(tempselected);
+						tempselected = selected;
+					}
+
+				} while (selected == -1 || button != MouseButton.LeftButton);
+
+				StartGame start = selected switch {
+					0 => SeaBattle.Start,
+					1 => MineSweeper.Start,
+					2 => TicTacToe.Start,
+					3 => Fifteens.Start,
+					4 => CowsBulls.Start,
+					_ => () => { },
+				};
+
+				try {
+					start();
+				}
+				catch (ReturnToMenu) { }
+
+				selected = -1;
+				ReturnToMenu = false; // Сбрасываем флаг для следующего выбора игры
+			}
+		}
+	}
+}
