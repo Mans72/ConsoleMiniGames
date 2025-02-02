ae1c26c2d24fc0dc43cb4050ac83a69a19175ff3
Mans72 <romanmans12@ukr.net>
2025-02-02 13:57:26 +0100

    Create MouseClick.cs

---
+++ Types/MouseClick.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMiniGames.Types {
+
+	internal readonly struct MouseClick(XY position, MouseButton button) {
+		public XY Position { get; } = position;
+		public MouseButton Button { get; } = button;
+		public int PosX { get; } = position.X;
+		public int PosY { get; } = position.Y;
+	}
+}
